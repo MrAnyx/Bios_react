fcd243c576a495af2fee7b24f9e5eaff2422734f
MrAnyx <1ptitigre@gmail.com>
2019-12-08 18:32:18 +0100

    [add] style and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Components/Header'
+import './css/fonts.css'
 
 export default class App extends React.Component {
+
     render() {
         return (
             <Header/>
-		)		
+		)	
     }
 }
+
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import '../css/fonts.css'
 import '../css/header.css'
+import logo from '../assets/logo.png'
 
-export default class Header extends React.Component{
+export default class Header extends React.Component {
     render () {
         return (
-            <h1>Header</h1>
+            <header>
+                <div id="content_header">
+                    <div id="logo">
+                        <img src={logo} alt="Logo de la page"/>
+                    </div>
+                    <div id="links">
+                        <span className="link">Features</span>
+                        <span className="link">Feedback</span>
+                        <span id="dl_app">Download app</span>
+                    </div>
+                </div>
+            </header>
         )
     }
 }

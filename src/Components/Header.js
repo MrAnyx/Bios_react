078a27f5cfe6cd099550b2f63509eb7e3c7e4e6f
MrAnyx <1ptitigre@gmail.com>
2019-12-08 18:09:32 +0100

    [add] initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../css/header.css'
+
+export default class Header extends React.Component{
+    render () {
+        return (
+            <h1>Header</h1>
+        )
+    }
+}

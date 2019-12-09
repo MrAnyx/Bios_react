34b238072896cbe190a163b714b7ff3d52842443
MrAnyx <1ptitigre@gmail.com>
2019-12-09 13:22:34 +0100

    [fix] delete build folder + image card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,7 +6,9 @@ export default class Card extends React.Component {
         return (
             <div className="col-lg-8 col-xl-5 col-md-8 col-11 card mb-5 mx-md-5">
                 <div className="card-content">
-                    <div className="decoration"></div>
+                    <div className="decoration">
+                        <img src={this.props.image} alt="test"/>
+                    </div>
                     <div className="content">
                         <span className="date">{this.props.date}</span>
                         <h6 className="title pt-1">{this.props.titre}</h6>
